be4df53e12d73739e763f69fbdc4a712ef37d856
Arnold Udemba <udembao429@gmail.com>
2025-02-18 21:37:09 +0100

    Update errorController.js

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -12,7 +12,7 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  //Operational or trusted error: send message to client
+  //Operationall or trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
